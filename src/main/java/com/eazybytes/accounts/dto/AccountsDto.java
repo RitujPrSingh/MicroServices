93dc50cdf022e3e58f081b676a41eb6002b89788
ritujsingh <ritujsingh@192.168.1.7>
2024-07-14 12:50:36 +0530

    First Commit

---
+++ src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+	
+	private String name;
+	
+	private String email;
+	
+	private String mobileNumber;
+
+}
---
+++ src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+	
+	private Long accountNumber;
+	
+	private String accountType;
+	
+	private String branchAddress;
+
+}
